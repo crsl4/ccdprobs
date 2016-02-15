d46db2775d193b8d9ad646438c2ff61c4d53f548
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-02-15 17:12:24 -0600

    latex and r scripts for 4 taxa cats with more details

--- scripts/4taxa_cats_dogs.r
+++ scripts/4taxa_cats_dogs.r
@@ -1,22 +1,79 @@
+# r script to sample tree from ccdprobs, and sample branch lengths with TN,
+# and calculate importance weight
+# Claudia February 2016
 
 library(ape)
 source('branch-length.r')
 
-d=read.dna("../datasets/4taxa-cats.phy") #needs to be 4 taxa
-# as.character?
-tre=read.tree("../datasets/4taxa-cats.tre") # fixit: how to sample a tree? from 4-taxon-cats_ccdprobs.out?
-eta=0.5
-tre=read.tree(text="((1,2),3,4);")
+# ===========================================================================================
+# Functions
+# ===========================================================================================
+
+# 1-----x-----2
+# d1x = distance from 1 to parent x, similarly d2x
+# seq1.distj = jth column in seq1.dist matrix (for site j), similarly seq2.distj
+# Q = estimated matrix of rates
+# returns column of site likelihood
+siteLik = function(d1x,d2x,seq1.distj,seq2.distj, Q){
+    P1 = matrixExp(Q,d1x)
+    P2 = matrixExp(Q,d2x)
+    lik = rep(0,4)
+    for(i in 1:4){
+        lik[i] = P1[i,]%*%seq1.distj * P2[i,]%*%seq2.distj
+        #print(lik)
+    }
+    return (lik)
+}
+
+
+# estimates the seq dist at x (parent of 1 and 2)
+sequenceDist = function(d1x,d2x,seq1.dist,seq2.dist, Q){
+    nsites = length(seq1)
+    if(length(seq2) != nsites){
+        error()
+    }
+    nuc = c('a','c','g','t')
+    seqx = matrix(rep(0,nsites*4),nrow=4)
+    for(i in 1:nsites){
+        seqx[,i] = siteLik(d1x,d2x,seq1.dist[,i],seq2.dist[,i],Q) * Q$p
+        seqx[,i] = seqx[,i]/sum(seqx[,i])
+    }
+    #print(seqx)
+    return(seqx)
+}
+
+#warning if x (counts) with rows of zero (and also columns because error in density(tn)), also if few counts in one row/column
+checkMatCounts = function(x){
+    if((0 %in% rowSums(x)) || (0 %in% colSums(x)) || (any(rowSums(x)<5)) || (any(colSums(x)<5))){
+        warning("the counts matrix rows/columns of zero, or very few counts per row/column, TN simulation of branch length could have an error")
+    }
+}
+
+sampleTopQuartet = function(dat.tre){
+    u=runif(1,0,1)
+    if(u<dat.tre$V2[1]){
+        t = dat.tre$V1[1]
+        prob=dat.tre$V2[1]
+    } else if(u < dat.tre$V2[1]+dat.tre$V2[2]){
+        t = dat.tre$V1[2]
+        prob=dat.tre$V2[2]
+    } else {
+        t = dat.tre$V1[3]
+        prob=dat.tre$V2[3]
+    }
+    tre = read.tree(text=as.character(t))
+    print(write.tree(tre))
+    return (list(tre=tre,prob=prob))
+}
 
 # function to sample branch lengths for 4-taxon tree
-sampleBL = function(d,tre,eta=0.5){
-    thr = sample(1:4, size=3, replace=FALSE) # need to choose three taxa
-    fth = setdiff(1:4,thr)
-    seq1 = as.vector(unname(as.character(d[thr[1],]))) # fixit: need to make sure that 1,2 are sisters
-    tre$tip.label[tre$edge[which(tre$edge[,1]==sample(c(5,6),1) & tre$edge[,2] < 5),2]]
-    seq2 = as.vector(unname(as.character(d[thr[2],])))
-    seq3 = as.vector(unname(as.character(d[thr[3],])))
-    seq4 = as.vector(unname(as.character(d[fth,])))
+sampleBLQuartet = function(d,tre,eta=0.5){
+    sis = tre$tip.label[tre$edge[which(tre$edge[,1]==sample(c(5,6),1) & tre$edge[,2] < 5),2]] #works only for unrooted quartet
+    fth = setdiff(1:4,sis)
+    seq1 = as.vector(unname(as.character(d[as.numeric(sis[1]),])))
+    seq2 = as.vector(unname(as.character(d[as.numeric(sis[2]),])))
+    seq3 = as.vector(unname(as.character(d[fth[1],])))
+    seq4 = as.vector(unname(as.character(d[fth[2],])))
     nsites = length(seq1)
     if(length(seq2) != nsites && length(seq3) != nsites){
         error()
@@ -30,9 +87,9 @@ sampleBL = function(d,tre,eta=0.5){
         for(j in 1:n)
             out12[i,j] = sum(seq1==nuc[i] & seq2==nuc[j])
     print(out12)
-    # want to check if out is good for tn? fixit
-    # need to determine eta, and jc vs tn?
-    d12 = simulateBranchLength.jc(nsim=1,out12,eta=eta)
+    checkMatCounts(out12)
+
+    #d12 = simulateBranchLength.jc(nsim=1,out12,eta=eta)
     d12 = simulateBranchLength.tn(nsim=1, out12, eta=eta)
     print(d12)
 
@@ -41,8 +98,9 @@ sampleBL = function(d,tre,eta=0.5){
         for(j in 1:n)
             out13[i,j] = sum(seq1==nuc[i] & seq3==nuc[j])
     print(out13)
+    checkMatCounts(out13)
 
-    d13 = simulateBranchLength.jc(nsim=1,out13,eta=eta)
+    #d13 = simulateBranchLength.jc(nsim=1,out13,eta=eta)
     d13 = simulateBranchLength.tn(nsim=1, out13, eta=eta)
     print(d13)
 
@@ -51,8 +109,9 @@ sampleBL = function(d,tre,eta=0.5){
         for(j in 1:n)
             out23[i,j] = sum(seq2==nuc[i] & seq3==nuc[j])
     print(out23)
+    checkMatCounts(out23)
 
-    d23 = simulateBranchLength.jc(nsim=1,out23,eta=eta)
+    #d23 = simulateBranchLength.jc(nsim=1,out23,eta=eta)
     d23 = simulateBranchLength.tn(nsim=1, out23, eta=eta)
     print(d23)
 
@@ -61,13 +120,6 @@ sampleBL = function(d,tre,eta=0.5){
 
     print(d1x)
     print(d2x)
-
-
-    #need seq distribution at x: felsenstein algorithm
-    r = rep(1,6)
-    Q = optim.gtr(out12,r) #fixit: x=matrix of counts, which counts when there are 4 sequences?
-    #Q = randomQ(4) #fixit: for the moment
-    print(Q$Q$Q)
 
     #need seq1.dist and seq2.dist which are matrices
     seq1.dist = matrix(0,n,nsites)
@@ -115,19 +167,23 @@ sampleBL = function(d,tre,eta=0.5){
             seq4.dist[4,i] = 1
         }
     }
-    #print(seq3.dist)
-    #print(seq4.dist)
 
-    seqx = sequenceDist(d1x,d2x, seq1.dist, seq2.dist,Q$Q) #fixit: correct Q? lik, not P(x|1,2)
-    print(seqx)
+    #need seq distribution at x: felsenstein algorithm
+    r = rep(1,6)
+    Q = optim.gtr(out12,r)
+    print(Q$Q$Q)
+
+    seqx = sequenceDist(d1x,d2x, seq1.dist, seq2.dist,Q$Q)
+    #print(seqx)
 
     out34 = matrix(0,n,n) # distance between 3 and 4
     for(i in 1:n)
         for(j in 1:n)
             out34[i,j] = sum(seq3==nuc[i] & seq4==nuc[j])
     print(out34)
+    checkMatCounts(out34)
 
-    d34 = simulateBranchLength.jc(nsim=1,out34,eta=eta)
+    #d34 = simulateBranchLength.jc(nsim=1,out34,eta=eta)
     d34 = simulateBranchLength.tn(nsim=1, out34, eta=eta)
     print(d34)
 
@@ -137,8 +193,9 @@ sampleBL = function(d,tre,eta=0.5){
         out3x = out3x + seq3.dist[,i]%*%t(seqx[,i])
     }
     print(out3x)
+    checkMatCounts(out3x)
 
-    d3x = simulateBranchLength.jc(nsim=1,out3x,eta=eta)
+    #d3x = simulateBranchLength.jc(nsim=1,out3x,eta=eta)
     d3x = simulateBranchLength.tn(nsim=1, out3x, eta=eta)
     print(d3x)
 
@@ -147,8 +204,9 @@ sampleBL = function(d,tre,eta=0.5){
         out4x = out4x + seq4.dist[,i]%*%t(seqx[,i])
     }
     print(out4x)
+    checkMatCounts(out4x)
 
-    d4x = simulateBranchLength.jc(nsim=1,out4x,eta=eta)
+    #d4x = simulateBranchLength.jc(nsim=1,out4x,eta=eta)
     d4x = simulateBranchLength.tn(nsim=1, out4x, eta=eta)
     print(d4x)
 
@@ -159,43 +217,63 @@ sampleBL = function(d,tre,eta=0.5){
     print(paste("d12",d12,"d13",d13,"d23",d23,"d34",d34))
     print(paste("d1x",d1x,"d2x",d2x,"d3y",d3y,"d4y",d4y,"dxy",dxy))
 
-                                        # need to update new bl in tre
+    bl <- rep(0,5) #works only for unrooted quartet
+    ed1x = which(tre$edge[,2] == which(tre$tip.label == sis[1]))
+    ed2x = which(tre$edge[,2] == which(tre$tip.label == sis[2]))
+    ed3y = which(tre$edge[,2] == which(tre$tip.label == as.character(fth[1])))
+    ed4y = which(tre$edge[,2] == which(tre$tip.label == as.character(fth[2])))
+    bl[ed1x] = d1x
+    bl[ed2x] = d2x
+    bl[ed3y] = d3y
+    bl[ed4y] = d4y
+    ind = which(bl==0)
+    bl[ind] = dxy
+    print(bl)
+
+    # now, compute likelihood of quartet with bl
+    suma = 0
+    for(s in 1:nsites){
+        lik12 = siteLik(d1x,d2x,seq1.dist[,s],seq2.dist[,s],Q$Q)
+        lik34 = siteLik(d3y,d4y,seq3.dist[,s],seq4.dist[,s],Q$Q)
+        L = lik12 %*% t(lik34)
+        Pxy = matrixExp(Q$Q,dxy)
+        L2 = L*Pxy
+        Lik = Q$Q$p * L2
+        suma = suma+log(sum(Lik))
+    }
+
+    # we need to compute density(bl|top)
+    dens = 0.01 #fixit: how? with TN gamma?
+
+    return (list(bl=bl, loglik=suma, density=dens)) #would like to update bl tre inside, but not possible in R
 }
 
-# 1-----x-----2
-# d1x = distance from 1 to parent x, similarly d2x
-# seq1.distj = jth column in seq1.dist matrix (for site j), similarly seq2.distj
-# Q = matrix of rates
-# returns column of site likelihood
-siteLik = function(d1x,d2x,seq1.distj,seq2.distj, Q){
-    P1 = matrixExp(Q,d1x)
-    P2 = matrixExp(Q,d2x)
-    lik = rep(0,4)
-    for(i in 1:4){
-        lik[i] = P1[i,]%*%seq1.distj * P2[i,]%*%seq2.distj
-        print(lik)
-    }
-    return (lik)
+
+# ====================================================================================
+# Example
+# ====================================================================================
+
+
+d=read.dna("../datasets/4taxa-cats.phy") #needs to be 4 taxa
+dat.tre=read.table("../datasets/4taxa-cats_ccdprobs.out", header=FALSE)
+t=sampleTopQuartet(dat.tre)
+b=sampleBLQuartet(d,t$tre)
+tre$edge.length <- b$bl
+
+prior = 1/3 #fixit
+logw = log(prior)+b$loglik-log(t$prob)-log(b$density) #posterior = prior*lik, not normalized
+print(logw) #very small
+
+nreps = 100
+trees = rep(NA,nreps)
+logwv = rep(0,nreps)
+for(i in 1:nreps){
+    t=sampleTopQuartet(dat.tre)
+    b=sampleBLQuartet(d,t$tre)
+    logw = log(prior)+b$loglik-log(t$prob)-log(b$density)
+    print(logw)
+    trees[i] = write.tree(t$tre)
+    logwv[i] = logw
 }
-
-
-# estimates the seq dist at x (parent of 1 and 2)
-sequenceDist = function(d1x,d2x,seq1.dist,seq2.dist, Q){
-    nsites = length(seq1)
-    if(length(seq2) != nsites){
-        error()
-    }
-    nuc = c('a','c','g','t')
-    seqx = matrix(rep(0,nsites*4),nrow=4)
-
-    for(i in 1:nsites){
-        seqx[,i] = siteLik(d1x,d2x,seq1.dist[,i],seq2.dist[,i],Q)
-    }
-    print(seqx)
-    # fixit: this is merely the likelihood, we want P(x|1,2)
-}
-
-# list of doubts:
-# 1) JC vs TN? eta=0.5
-# 2) seq dist at x: how to estimate Q? which counts? the siteLik function is merely the lik, we want P(x|1,2)
-# 3) we have ccdprobs, how to sample tree? from which output file?
+data=data.frame(trees=trees,logw=logwv)
+summary(data)
