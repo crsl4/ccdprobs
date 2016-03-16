29d9e37317852490028942a38a4d44d89fcecce7
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-03-15 20:13:25 -0500

    simulated data in negative BL

--- scripts/simulated_negBL.r
+++ scripts/simulated_negBL.r
@@ -9,8 +9,9 @@ source('4taxa_functions.r')
 nsites = 1500
 t="((1,2),3,4);"
 tre=read.tree(text=t)
-dxy0=0.03
-dxy0=0.5
+dxy0=0.03 ##small
+dxy0=0.5 ##big
+dxy0=0.003 ##smaller
 branch.length = c(dxy0,0.11,0.078,0.091,0.098) #bl as birds mb: dxy,d1x,d2x,d3y,d4y
 tre$edge.length <- branch.length
 nuc <- c('a','c','g','t')
@@ -102,26 +103,25 @@ data_big=data
 err_big=err
 data_small=data
 err_small=err
-
 save(data_big, err_big, data_small, err_small, file="simulatedNegBL.Rda")
+save(data,file="simulatedNegBL_smaller.Rda")
 
-## todo: finish this part, get to simplex, compare
-## later, go to cov section, and plot histograms, are they different to birds?
-## then, modify dxy to get different simplex and hist to compare
+load("simulatedNegBL.Rda")
+data=data_big
+data=data_small
 
-
-save(data,file=paste0("negBL_",who,"tree3.Rda"))
+head(data)
 which(data$d1x<0)
 which(data$d2x<0)
 which(data$d3x<0)
 which(data$d3y<0)
 which(data$d4y<0)
-which(data$dxy<0) #only dxy is negative as expected
+which(data$dxy<0)
 negLines = which(data$dxy<0)
 length(negLines) #407
 
 library(ggplot2)
-pl <- ggplot(data,aes(x=1:nreps,y=dxy))+
+pl <- ggplot(data,aes(x=1:length(dxy),y=dxy))+
     geom_point(aes(color=trees))
 plot(pl)
 
@@ -147,7 +147,7 @@ pl3 <- ggtern(data=newdat,aes(d3x,d4x,d34)) +
     geom_Tline(Tintercept=0.5) + geom_Rline(Rintercept=0.5) + geom_Lline(Lintercept=0.5)
 plot(pl3)
 
-pdf(paste0("simplex_",who,"_tree3.pdf"))
+pdf(paste0("simplex_simSmaller.pdf"))
 plot(pl3)
 dev.off()
 
@@ -168,3 +168,71 @@ pl4 <- ggtern(data=newdat2,aes(d12,d23,d13)) +
     geom_point(aes(fill=trees),shape=21,size=1)+
     geom_Tline(Tintercept=0.5) + geom_Rline(Rintercept=0.5) + geom_Lline(Lintercept=0.5)
 plot(pl4)
+
+pdf(paste0("simplex_simSmaller2.pdf"))
+plot(pl4)
+dev.off()
+
+## ------------------------------------------------------
+## histograms BL
+## ------------------------------------------------------
+data <- within(data,var12 <- alpha12/beta12^2)
+data <- within(data,var23 <- alpha23/beta23^2)
+data <- within(data,var13 <- alpha13/beta13^2)
+data <- within(data,var4x <- alpha4x/beta4x^2)
+data <- within(data,var34 <- alpha34/beta34^2)
+data <- within(data,var3x <- 0.25*(var23+var13+var12))
+
+data <- within(data,cov1x.2x <- 0.25*(var12-var13-var23))
+data <- within(data,cov2x.3x <- 0.25*(var23-var12-var13))
+data <- within(data,cov1x.3x <- 0.25*(var13-var12-var23))
+data <- within(data,cov3y.xy <- 0.25*(var34-var3x-var4x))
+data <- within(data,cov4y.xy <- 0.25*(var4x-var34-var3x))
+data <- within(data,cov3y.4y <- 0.25*(var3x-var34-var4x))
+summary(data)
+
+## for tree1
+data1 <- data[!is.na(data$trees),]
+summary(data1)
+
+## birds tree1:
+d1x = branch.length[2]
+d2x = branch.length[3]
+d3y = branch.length[4]
+d4y = branch.length[5]
+dxy = dxy0
+
+d12=d1x+d2x
+d13=d3y+dxy+d1x
+d23=d3y+dxy+d2x
+d34=d3y+d4y
+d3x=d3y+dxy
+d4x=d4y+dxy
+
+dat=data1
+## change file name and main title
+pdf("simSmaller_histBL.pdf", height=6,width=15)
+layout(m=matrix(c(1,2,3,4,5,6,7,8,9,10,11,12), nrow=2))
+hist(dat$d12, main="Simulated (1,2),3,4")
+abline(v=d12,col="red")
+hist(dat$d13)
+abline(v=d13,col="red")
+hist(dat$d23)
+abline(v=d23,col="red")
+hist(dat$d34)
+abline(v=d34,col="red")
+hist(dat$d3x)
+abline(v=d3x,col="red")
+hist(dat$d4x)
+abline(v=d4x,col="red")
+hist(dat$d1x)
+abline(v=d1x,col="red")
+hist(dat$d2x)
+abline(v=d2x,col="red")
+hist(dat$d3y)
+abline(v=d3y,col="red")
+hist(dat$d4y)
+abline(v=d4y,col="red")
+hist(dat$dxy)
+abline(v=dxy,col="red")
+dev.off()
