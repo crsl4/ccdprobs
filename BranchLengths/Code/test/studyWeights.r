8418edc3cc5b1270c850f0c25bc190bf52eadd85
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-05-26 13:37:56 -0500

    run multinormal case for simulated data to compare with R

--- BranchLengths/Code/test/studyWeights.r
+++ BranchLengths/Code/test/studyWeights.r
@@ -3,7 +3,7 @@
 ## Claudia May 2016
 
 seed = 646
-dat = read.table(paste0("logw",seed,".txt"))
+dat = read.table(paste0("logw",seed,"_norm.txt"))
 head(dat)
 summary(dat$V1)
 mean(dat$V1)
@@ -11,3 +11,9 @@ mean(dat$V1)
 logw = dat$V1 - mean(dat$V1)
 summary(logw)
 hist(logw)
+w = exp(logw) / sum(exp(logw))
+summary(w)
+length(w[w>0.01]) ## 12
+hist(w)
+plot(1:length(w),cumsum(rev(sort(w))))
+(1/sum(w^2))/1000 ## 0.0413
