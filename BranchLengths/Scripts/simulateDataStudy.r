81d9029f6091b346b865fd65f8d2d3099bcde027
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-06-07 20:23:38 -0500

    new changes to simulate study

--- BranchLengths/Scripts/simulateDataStudy.r
+++ BranchLengths/Scripts/simulateDataStudy.r
@@ -11,5 +11,14 @@ nsites = 1500
 alpha = 1000
 
 ## need to put the tree from cats dogs, and then take subsets for all number of taxa
+set.seed(seed)
 tree.text = "((1:0.11,2:0.078):0.03,3:0.098,4:0.091);"
+tree = read.tree(text=tree.text)
+Q = randomQ(4,rescale=TRUE)
 
+write(paste("ntaxa =",ntax, "----------------"),file="../Examples/Simulations/settings.txt",append=TRUE)
+write(paste("tree =",tree.text),file="../Examples/Simulations/settings.txt",append=TRUE)
+write(paste("rates =",Q$r),file="../Examples/Simulations/settings.txt",append=TRUE)
+write(paste("probs =",Q$r),file="../Examples/Simulations/settings.txt",append=TRUE)
+
+x = simData(tree,nsites,Q,alpha)
