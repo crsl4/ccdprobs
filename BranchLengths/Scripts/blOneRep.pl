15795e0f4877adbd9d8456a09bdb04eb32d0dffe
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-06-08 22:07:17 -0500

    perl script for simulation

---
+++ BranchLengths/Scripts/blOneRep.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl
+
+# perl script to run one replicate of bl analysis:
+# - simulate fasta data for ntax=3,..,12
+# for each ntax:
+# - run bl on each data (time it)
+# - run studyWeights.r on created logw.txt
+# - rename logw.txt to current rep and ntax
+# this will create a file weights.txt with one row per replicate
+# warning: need to run inside BranchLength/Scripts
+
+use Getopt::Long;
+use File::Path qw( make_path );
+use strict;
+use warnings;
+use Carp;
+
+
+# ================= parameters ======================
+# Number of replicate of the whole process
+my $irep = 1;
+my $longseed = time;
+my $seed = substr($longseed, -5);
+
+# -------------- read arguments from command-line -----------------------
+GetOptions( 'irep=i' => \$irep,
+    );
+
+my $logfile = "blOneRep$irep.txt";
+open FHlog, ">> $logfile";
+print FHlog "============================================\n";
+print FHlog "blOneRep for replicate $irep\n";
+close FHlog;
+system("date >> $logfile");
+system("hostname >> $logfile");
+
+
+# 1) Simulate the data for each ntax
+open FHlog, ">> $logfile";
+print FHlog "running simulateDataStudy.r\n";
+my $Rcmd = "Rscript simulateDataStudy.r $irep $seed";
+print FHlog "$Rcmd\n";
+close FHlog;
+system($Rcmd);
+
+# 2) read settings from settings$irep.txt
+my $setFile = "../Examples/Simulations/settings$irep.txt";
+open FHlog, ">> $logfile";
+print FHlog "reading file $setFile\n";
+my @trees = `grep "tree =" $setFile`;
+my @rates = `grep "rates =" $setFile`;
+my @probs = `grep "probs =" $setFile`;
+
+my @splittree;
+my @splitrates;
+my @splitprobs;
+my $ntaxtree;
+my $ntaxrates;
+my $ntaxprobs;
+my $blcmd;
+my $start;
+my $duration;
+my $newfile;
+
+# one replicate will run analysis for each ntaxa
+foreach my $ntax (3,4,6,12){
+    print FHlog "-----------------ntaxa = $ntax------------------\n";
+    print "ntaxa = $ntax, ";
+    # the actual tree, rates and probs is the third element after splitting by space
+    # maybe there is a better way to do this step
+    @splittree = split / /, $trees[$ntax-3];
+    @splitrates = split / /, $rates[$ntax-3];
+    @splitprobs = split / /, $probs[$ntax-3];
+    $ntaxtree = $splittree[2];
+    $ntaxrates = $splitrates[2];
+    $ntaxprobs = $splitprobs[2];
+    chomp $ntaxtree;
+    chomp $ntaxrates;
+    chomp $ntaxprobs;
+
+    $seed = $seed +($ntax-3)*int(rand(1000));
+
+    $blcmd = "../Code/test/bl -f ../Data/sim$ntax.fasta -t \"$ntaxtree\" -q $ntaxrates -p $ntaxprobs --mvnormal -s $seed >& out$ntax";
+    print "running bl\n";
+    print FHlog "$blcmd\n";
+    $start = time;
+    system($blcmd);
+    $duration = time - $start;
+
+    $Rcmd = "Rscript studyWeights.r $ntax $irep $duration $seed";
+    print FHlog "$Rcmd\n";
+    system($Rcmd);
+
+    $newfile = "logw_${ntax}_${irep}.txt";
+    `mv logw.txt $newfile`;
+}
+
--- BranchLengths/Scripts/studyWeights.r
+++ BranchLengths/Scripts/studyWeights.r
@@ -3,6 +3,21 @@
 ## Claudia May 2016
 ## Modified by Bret on June 1, 2016
 ## Modified by Claudia to save info on table for simulation study (6/7/16)
+## run: R CMD BATCH studyWeights.r ntax rep time seed
+## will append to weights.txt for all replicates
+
+args = commandArgs(trailingOnly=TRUE)
+if(length(args) == 0){
+    ntax = 0
+    rep = 0
+    time = 0
+    seed = 0
+}else{
+    ntax = strtoi(args[1])
+    rep = strtoi(args[2])
+    time = strtoi(args[3])
+    seed = strtoi(args[4])
+}
 
 
 dat = read.csv("logw.txt",header=FALSE)
@@ -17,4 +32,4 @@ nrep = length(w)
 ess = 1/sum(w^2)
 essp = ess/nrep
 
-write.table(data.frame(mi,m,ma,nbig,nrep,ess,essp), file="weights.txt", append=TRUE, row.names=FALSE, col.names=FALSE, sep=",")
+write.table(data.frame(ntax,rep,time,seed,mi,m,ma,nbig,nrep,ess,essp), file="weights.txt", append=TRUE, row.names=FALSE, col.names=FALSE, sep=",")
