7ff2851fc3b8d627b6bd907b289eac489caf815d
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-07-22 16:47:33 -0500

    modified freq.r

--- Bistro/Scripts/freq.r
+++ Bistro/Scripts/freq.r
@@ -1,4 +1,5 @@
 ## R code to compare the frequency of trees
+## to mrbayes
 
 library(ape)
 dat <- read.table("run15.txt", header=TRUE)
@@ -8,10 +9,13 @@ logw = logweight - max(logweight)
 w = exp(logw) / sum(exp(logw))
 dat$w <- w
 
+1/sum(w^2) ## does this match?
+
 means=with(dat, sapply( split( w,tree ),sum ) )
+
 dat$mean = means[8]
 head(dat)
 dat$indicator = dat$tree == levels(dat$tree)[8]
 v = sum((dat$w*dat$indicator - dat$mean)^2)/100
 
-1/sum(w^2)
+
