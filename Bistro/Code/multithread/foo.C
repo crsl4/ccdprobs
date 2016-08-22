e040d2a64eff2f7744f9115381f38e63765c79d8
Bret Larget <bret.larget@wisc.edu>
2016-08-22 12:35:21 -0500

    multiple rng example

--- Bistro/Code/multithread/foo.C
+++ Bistro/Code/multithread/foo.C
@@ -2,15 +2,17 @@
 #include <iomanip>
 #include <thread>
 #include <vector>
+#include <random>
 
 using namespace std;
 
-void thread_function(int id,int n,vector<int>& v)
+void thread_function(mt19937_64& rng,int id,int n,vector<int>& v)
 {
   cout << "Beginning thread " << id << ", also known as " << this_thread::get_id() << endl;
   for ( int i=0; i<n; ++i )
   {
-    v[n*id + i] = id;
+    uniform_int_distribution<> rint(0,99);
+    v[n*id + i] = rint(rng);
   }
   cout << "Ending thread " << id << endl;
 }
@@ -20,12 +22,28 @@ int main()
   unsigned int cores = thread::hardware_concurrency();
   cout << cores << " cores." << endl;
 
+  random_device rd;
+  unsigned int initial_seed = rd();
+//  unsigned int initial_seed = 747705983;
+  cout << "Seed = " << initial_seed << endl;
+  minstd_rand seed_rng(initial_seed);
+  uniform_int_distribution<> rint(0,4294967295);
+  vector<unsigned int> seeds(cores);
+  vector<mt19937_64*> rng;
+  for ( vector<unsigned int>::iterator p=seeds.begin(); p!=seeds.end(); ++p )
+  {
+    *p = rint(seed_rng);
+    rng.push_back( new mt19937_64(*p) );
+  }
+  for ( vector<unsigned int>::iterator p=seeds.begin(); p!=seeds.end(); ++p )
+    cout << setw(15) << *p << endl;
+
   int n = 10;
   vector<int> v(cores*n);
   vector<thread> threads;
   for ( int i=0; i<cores; ++i )
   {
-    threads.push_back(thread(thread_function,i,n,ref(v)));
+    threads.push_back(thread(thread_function,ref(*(rng[i])),i,n,ref(v)));
   }
   for ( int i=0; i<cores; ++i )
     threads.at(i).join();
