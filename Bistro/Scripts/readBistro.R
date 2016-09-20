bb2fb7679261a960dc4ad7f47e44e5a699bfbbe5
Bret Larget <bret.larget@wisc.edu>
2016-09-20 14:13:04 -0500

    changed readBistro

--- Bistro/Scripts/readBistro.R
+++ Bistro/Scripts/readBistro.R
@@ -1,26 +1,40 @@
 ## read in the different .out files
 ## kluge here assumes 4 cores with 0-249, 250-499, 500-749, 750-999
 
-readBistro = function (stem)
+readBistro = function(stem)
 {
-    bistro.1 = read.table(paste(stem,"---0-249.out",sep=""),header=TRUE)
-    bistro.2 = read.table(paste(stem,"---250-499.out",sep=""),header=TRUE)
-    bistro.3 = read.table(paste(stem,"---500-749.out",sep=""),header=TRUE)
-    bistro.4 = read.table(paste(stem,"---750-999.out",sep=""),header=TRUE)
-    bistro = rbind(bistro.1,bistro.2,bistro.3,bistro.4)
-    rm(bistro.1,bistro.2,bistro.3,bistro.4)
+    files = list.files(pattern=paste0("^",stem,"---.*\\.out"))
+##    bistro.1 = read.table(paste(stem,"---0-249.out",sep=""),header=TRUE)
+##    bistro.2 = read.table(paste(stem,"---250-499.out",sep=""),header=TRUE)
+##    bistro.3 = read.table(paste(stem,"---500-749.out",sep=""),header=TRUE)
+##    bistro.4 = read.table(paste(stem,"---750-999.out",sep=""),header=TRUE)
+##    bistro = rbind(bistro.1,bistro.2,bistro.3,bistro.4)
+    bistro = data.frame()
+    for ( f in files ) {
+        temp = read.table(f,header=TRUE)
+        bistro = rbind(bistro,temp)
+        rm(temp)
+    }
+#    rm(bistro.1,bistro.2,bistro.3,bistro.4)
     bistro$w = with(bistro, exp(logWt - max(logWt)))
     bistro$w = with(bistro, w/sum(w))
     bistro$logQ = with(bistro, logl + logPrior - logTop - logProp - logWt)
+    return ( bistro )
+}
+
+plotBistro = function(bistro) {
     require(ggplot2)
     require(viridis)
-    viridis.scale = viridis(n=length(levels(bistro$tree)))
-##  my.plot = ggplot(bistro,aes(x=logl+logPrior,y=logQ+logTop+logProp,color=w,shape=tree)) + geom_point() + scale_color_viridis() + coord_fixed()
-    my.plot = ggplot(bistro,aes(x=logl+logPrior,y=logQ+logTop+logProp,color=w)) +
-        geom_point() +
-        scale_color_viridis() +
-##        guides(color=FALSE) +
-        coord_fixed()
+    temp.tree = as.character(bistro$tree)
+    if ( length(levels(bistro$tree)) > 6 ) {
+        tab = with(bistro, rev(sort(table(tree))))
+        top.trees = names(tab)[1:5]
+        temp.tree[ !(bistro$tree %in% top.trees) ] = "other"
+    }
+    bistro$Tree = factor(temp.tree)
+    rm(temp.tree)
+    viridis.scale = viridis(n=length(levels(bistro$Tree)))
+    my.plot = ggplot(bistro,aes(x=logl+logPrior,y=logQ+logTop+logProp,color=w,shape=Tree)) + geom_point() + scale_color_viridis() + coord_fixed()
     plot(my.plot)
-    return(bistro)
+    return(invisible(my.plot))
 }
