b8caf41e0c98cb8fc0375d9135ed950065dc1da7
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-11-30 18:47:43 -0600

    more tests with longer chain

--- Bistro/Code/bistro/main-ccd.C
+++ Bistro/Code/bistro/main-ccd.C
@@ -2,9 +2,10 @@
 // Bret Larget and Claudia Solis-Lemus
 // Copyright 2016
 // May 20, 2016
+// will only run ccdprobs on bootstrap sample (with parsimony and lik weights)
 
-#define MCMC_Q_BURN 100000
-#define MCMC_Q_SAMPLE 1000000
+#define MCMC_Q_BURN 100
+#define MCMC_Q_SAMPLE 1000
 
 
 #include <iostream>
@@ -128,8 +129,8 @@ void randomTrees(int coreID, int indStart, int indEnd, vector<double>& logwt, do
        double scale = 1;
        if( parameters.getFixedQ() )
 	 scale = 10000000;
-       VectorXd p_star = dirichletProposalDensityScale(q_init.getStationaryP(), scale*q_init.getMcmcVarP(), logQ, rng);
-       VectorXd s_star = dirichletProposalDensityScale(q_init.getSymmetricQP(), scale*q_init.getMcmcVarQP(), logQ, rng);
+       VectorXd p_star = dirichletProposalDensityScale(q_init.getStationaryP(), q_init.getMcmcVarP(), logQ, rng);
+       VectorXd s_star = dirichletProposalDensityScale(q_init.getSymmetricQP(), q_init.getMcmcVarQP(), logQ, rng);
 
        if( parameters.getFixedQ() )
 	 logQ = 0;
@@ -286,24 +287,11 @@ int main(int argc, char* argv[])
   vector<double> s_init(6,0.1);
   s_init[1] = 0.3;
   s_init[4] = 0.3;
-  p_init[0] = 0.2429295; //mb values for cats-dogs
-  p_init[1] =  0.2356097;
-  p_init[2] = 0.2067433;
-  p_init[3] = 0.3147175;
-
-  s_init[0] = 0.047201519;
-  s_init[1] = 0.293928352;
-  s_init[2] = 0.068691153;
-  s_init[3] = 0.012274385;
-  s_init[4] = 0.569989255;
-  s_init[5] = 0.007915336;
 
   QMatrix q_init(p_init,s_init);
-//  jctree.setInitialEdgeLengths(0.1); //0.1 ignored, and branches set inside close to true for cats-dogs CCLT
 
  // burnin
   cerr << "burn-in:" << endl;
-  //  q_init.mcmc(alignment,jctree,(MCMC_Q_BURN),alignment.getNumSites(),rng);
   jctree.mcmc(q_init,alignment,(MCMC_Q_BURN),alignment.getNumSites(),rng, true);
   cerr << endl << " done." << endl;
   cout << "JC tree: " << endl;
@@ -311,7 +299,6 @@ int main(int argc, char* argv[])
 
   // mcmc to get final Q
   cerr << "sampling:" << endl;
-  //  q_init.mcmc(alignment,jctree,(MCMC_Q_SAMPLE),alignment.getNumSites(),rng);
   jctree.mcmc(q_init,alignment,(MCMC_Q_SAMPLE),alignment.getNumSites(),rng, false);
   cerr << endl << " done." << endl;
   cout << "JC tree: " << endl;
@@ -482,189 +469,6 @@ int main(int argc, char* argv[])
   tmap.open(loglikTmapFile);
   ccdLogLik.writePairCount(tmap);
   tmap.close();
-  milliseconds ms10 = duration_cast< milliseconds >( system_clock::now().time_since_epoch() );
-
-  if ( parameters.getNumRandom() > 0 )
-  {
-    int numRandom = parameters.getNumRandom();
-
-    unsigned int cores;
-    if( parameters.getNumThreads() == 0 )
-      {
-	cores = thread::hardware_concurrency();
-	if( cores == 0 ) //hardware_concurrency could return 0
-	  cores = 1;
-      }
-    else
-      cores = parameters.getNumThreads();
-    if( numRandom < cores )
-      cores = numRandom; // do not create more threads than trees
-    // i want to check that cores not > than hardware_concurrency?
-    cerr << "Generating " << numRandom << " random trees in " << cores << " cores:" << endl;
-    int div = numRandom / cores;
-    int remainder = numRandom % cores;
-    vector<int> startTreeNumber(cores);
-    for ( int i=0; i<cores; ++i )
-      startTreeNumber[i] = i*div + (i < remainder ? i : remainder);
-    startTreeNumber.push_back(numRandom);
-
-    // generating the seeds for each core
-    uniform_int_distribution<> rint_orig(0,4294967295);
-    unsigned int initial_seed = rint_orig(rng);
-    cout << "Seed = " << initial_seed << endl;
-    minstd_rand seed_rng(initial_seed);
-    uniform_int_distribution<> rint(0,4294967295);
-    vector<unsigned int> seeds(cores);
-    vector<mt19937_64*> vrng;
-    for ( vector<unsigned int>::iterator p=seeds.begin(); p!=seeds.end(); ++p )
-      {
-    	*p = rint(seed_rng);
-    	vrng.push_back( new mt19937_64(*p) );
-      }
-    cout << "Seeds per core: " << endl;
-    for ( vector<unsigned int>::iterator p=seeds.begin(); p!=seeds.end(); ++p )
-      cout << setw(15) << *p << endl;
-
-    vector<thread> threads;
-    vector< multimap<string,double> > topologymm(cores); //vector of multimaps
-    vector< vector<double> > logwt0(cores);
-    vector<double> maxLogW(cores); //vector of maxlogweight
-    cerr << "jointMLE " << parameters.getJointMLE() << endl;
-    cerr << "fixedQ " << parameters.getFixedQ() << endl;
-    cerr << "loglik weights " << parameters.getLoglikWt() << endl;
-    cerr << "eta " << parameters.getEta() << endl;
-
-    for ( int i=0; i<cores; ++i )
-    {
-      cerr << "core = " << i << endl;
-      if ( !parameters.getReweight() )
-	threads.push_back(thread(randomTrees<int>,i,startTreeNumber[i], startTreeNumber[i+1], ref(logwt0[i]), ref(maxLogW[i]), ccd, ref(*(vrng[i])), ref(alignment), ref(gtrDistanceMatrix), ref(model), ref(parameters), ref(topologymm[i])));
-      else
-	if( parameters.getLoglikWt() )
-	  threads.push_back(thread(randomTrees<double>,i,startTreeNumber[i], startTreeNumber[i+1], ref(logwt0[i]), ref(maxLogW[i]), ccdLogLik, ref(*(vrng[i])), ref(alignment), ref(gtrDistanceMatrix), ref(model), ref(parameters), ref(topologymm[i])));
-	else
-	  threads.push_back(thread(randomTrees<double>,i,startTreeNumber[i], startTreeNumber[i+1], ref(logwt0[i]), ref(maxLogW[i]), ccdParsimony, ref(*(vrng[i])), ref(alignment), ref(gtrDistanceMatrix), ref(model), ref(parameters), ref(topologymm[i])));
-    }
-
-    for(auto &t : threads){
-      t.join();
-    }
-    // for ( int i=0; i<cores; ++i )
-    //   threads.at(i).join();
-    cerr << endl << "done." << endl;
-
-    // combine all maxlogweight
-    double maxLogWeight = maxLogW[0];
-    for(int i=1; i<cores; ++i)
-      {
-	if(maxLogW[i] > maxLogWeight)
-	  maxLogWeight = maxLogW[i];
-      }
-    cerr << "maxLogWeight: " << maxLogWeight << endl;
-
-    // combine vector of all logwt
-    vector<double> logwt(numRandom,0);
-    int k = 0;
-    for( vector< vector<double> >::iterator p=logwt0.begin(); p != logwt0.end(); ++p)
-      {
-	for(vector<double>::iterator q=(*p).begin(); q != (*p).end(); ++q)
-	  {
-	    logwt[k] = *q;
-	    //cerr << "logwt[" << k << "]= " << logwt[k] << endl;
-	    ++k;
-	  }
-      }
-
-    // combine topology maps to one
-    multimap<string,double> topologyToLogweightMMap;
-    for ( vector< multimap<string,double>>::iterator p=topologymm.begin(); p!=topologymm.end(); ++p) //for every multimap in topologymm
-      {
-	for ( multimap<string,double >::iterator q=(*p).begin(); q!= (*p).end(); ++q) // traverse this multimap (*p)
-	  {
-	    topologyToLogweightMMap.insert( pair<string,double>(q->first,q->second) ) ;
-	  }
-      }
-
-    vector<double> wt(numRandom,0);
-    double sum=0;
-    for ( int k=0; k<numRandom; ++k )
-    {
-      //      cerr << "logwt[k]: " << logwt[k] << "for k= " << k << endl;
-      wt[k] = exp(logwt[k] - maxLogWeight);
-      //cerr << "wt[k]: " << wt[k] << endl;
-      sum += wt[k];
-    }
-    double essInverse=0;
-    for ( int k=0; k<numRandom; ++k )
-    {
-      wt[k] /= sum;
-      essInverse += wt[k]*wt[k];
-    }
-    cout << "ESS = " << fixed << setprecision(2) << 1.0/essInverse << ", or "
-	 << setprecision(2) << 100.0 / essInverse / numRandom << " percent." << endl;
-    cerr << "ESS = " << fixed << setprecision(2) << 1.0/essInverse << ", or "
-	 << setprecision(2) << 100.0 / essInverse / numRandom << " percent." << endl;
-
-    // substract max from multimap
-    for ( multimap<string,double >::iterator p=topologyToLogweightMMap.begin(); p!= topologyToLogweightMMap.end(); ++p) {
-      p->second -= maxLogWeight; // need to test this
-    }
-
-    // map from topology to unnormalized weight
-    map<string,double> topologyToUnnormalizedWeightMap;
-    for ( multimap<string,double >::iterator p=topologyToLogweightMMap.begin(); p!= topologyToLogweightMMap.end(); ++p) {
-      pair< multimap<string,double >::iterator,multimap<string,double >::iterator> ret = topologyToLogweightMMap.equal_range(p->first);
-      double sumWt = 0;
-      for ( multimap<string,double >::iterator q=ret.first; q!= ret.second; ++q) {
-	sumWt += exp(q->second);
-      }
-      //cout << "topology: " << (p->first) << " and sum of weights " << sumWt << endl;
-      topologyToUnnormalizedWeightMap[ p->first ] = sumWt;
-    }
-
-    CCDProbs<double> splits(topologyToUnnormalizedWeightMap,taxaNumbers,taxaNames);
-    string splitsWeightsFile = parameters.getOutFileRoot() + ".splits";
-    ofstream splitsWt(splitsWeightsFile.c_str());
-    splits.writeCladeCountOrdered(splitsWt, essInverse);
-    splitsWt.close();
-
-    string topologyPPFile = parameters.getOutFileRoot() + ".topPP";
-    ofstream topPP(topologyPPFile.c_str());
-
-    vector< pair<string,double> > v;
-    copy(topologyToUnnormalizedWeightMap.begin(), topologyToUnnormalizedWeightMap.end(), back_inserter(v));
-    sort(v.begin(), v.end(), comparePairStringDouble);
-    double prob;
-    double se;
-    topPP << "tree prob crudeSE(ESS=" << fixed << setprecision(2) << 1.0/essInverse << ")" << endl;
-
-    for (vector<pair<string,double>>::reverse_iterator i = v.rbegin(); i != v.rend(); ++i ) {
-      prob = i->second / sum;
-      se = sqrt(prob * (1-prob) * essInverse);
-      topPP << (i->first) << " " << fixed << setprecision(4) << prob << " " << se << endl;
-    }
-
-    // for ( map<string,double >::iterator p=topologyToUnnormalizedWeightMap.begin(); p!= topologyToUnnormalizedWeightMap.end(); ++p) {
-    //   prob = (p->second)/sum;
-    //   se = sqrt(prob * (1-prob) * essInverse);
-    //   topPP << (p->first) << fixed << setprecision(4) << prob << " " << se << endl;
-    // }
-  }
-
-  milliseconds ms11 = duration_cast< milliseconds >( system_clock::now().time_since_epoch() );
-
-  cout << "Times: " << endl;
-  cout << ms1.count() - ms0.count() << endl;
-  cout << ms2.count() - ms1.count() << endl;
-  cout << ms3.count() - ms2.count() << endl;
-  cout << ms4.count() - ms3.count() << endl;
-  cout << ms5.count() - ms4.count() << endl;
-  cout << ms6.count() - ms5.count() << endl;
-  cout << ms7.count() - ms6.count() << endl;
-  cout << ms8.count() - ms7.count() << endl;
-  cout << ms9.count() - ms8.count() << endl;
-  cout << ms10.count() - ms9.count() << endl;
-  cout << ms11.count() - ms10.count() << endl;
 
   return 0;
 }
