b82da6622c78478b33604442817f5ae5c33f38e8
Bret Larget <bret.larget@wisc.edu>
2017-04-17 09:25:56 -0500

    truncated normal

--- Bistro/Examples/Sim-16/foo.R
+++ Bistro/Examples/Sim-16/foo.R
@@ -4,6 +4,13 @@ ggplot(foo, aes(x=t,y=logl)) +
   geom_line() +
   geom_smooth(method="lm",se=FALSE)
 
+fit = lm(logl ~ t + I(t^2), data=foo)
+foo = foo %>%
+  mutate(fitted = fitted(fit))
+ggplot(foo, aes(x=t,y=logl)) +
+  geom_line() +
+  geom_line(aes(y=fitted),color="red")
+
 ggplot(foo, aes(x=t,y=dlogl)) +
   geom_line()
 
@@ -13,13 +20,13 @@ ggplot(foo, aes(x=t,y=ddlogl)) +
 require(dplyr)
 foo = foo %>%
   mutate(y = exp(logl-max(logl))) %>%
-  mutate(y = y / sum(y) / 0.001) %>%
+  mutate(y = y / sum(y) / 0.00005) %>%
   mutate(logy = log(y))
 
 ggplot(foo, aes(x=t,y=logy)) +
-  geom_line() +
-  xlim(c(0,0.01)) +
-  ylim(c(-10,10))
+  geom_line()
+#  xlim(c(0,0.01)) +
+#  ylim(c(-10,10))
 
 mu = with(foo, sum(t*y)/sum(y))
 alpha = 1 + 1/mu
@@ -27,15 +34,13 @@ alpha = 1 + 1/mu
 df = data.frame(x=seq(0,0.005,length.out=501))
 df = df %>%
   mutate(y=alpha / (x+1)^(alpha+1)) %>%
-  mutate(y=y/sum(y) / (0.005/500)) %>%
-  mutate(z=dexp(x,1/mu))
+  mutate(y=y/sum(y) / (0.005/500))
 
 with(df, sum(x*y)/ sum(y) )
 
 ggplot(foo, aes(x=t,y=y)) +
   geom_point() +
-  geom_point(aes(x=x,y=y), data=df, color="blue") +
-  xlim(c(0,0.005))
+  geom_point(aes(x=x,y=y), data=df, color="blue")
 
 foo2 = read.csv("foo2.csv")
 ggplot(foo2, aes(x=t,y=logl)) +
