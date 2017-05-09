8f7fedceb2ebb57ff7ec740063657bd6bb5b66e1
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2017-05-09 11:23:46 -0500

    bistro024 fixed tree

---
+++ Bistro/Examples/datasets/summarySampler.r
@@ -0,0 +1,10 @@
+tab = read.table("bistro024---0-249.sampler", header=TRUE)
+str(tab)
+summary(tab)
+
+tab2 = read.table("bistro043---0-249.sampler", header=TRUE)
+summary(tab2)
+str(tab2)
+
+tab3 = read.table("bistroFixT024---0-249.sampler", header=TRUE)
+summary(tab3)
